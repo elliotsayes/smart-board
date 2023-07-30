90ad009f85996a9ab1fd8b7a41108e153f26fed7
Elliot Sayes <elliot@sayes.cloud>
2023-07-30 22:59:23 +0800

    Main page test

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import './App.css'
 import TimelinePicker from './components/TimelinePicker'
 // import TimelineTest from './components/TimelineTest'
 
+const items10k = Array.from({ length: 1_000 }, (_, i) => ({
+  id: i,
+  start: new Date(1800 + Math.floor(i / (12 * 28)), (i * 28) % (12), 1 + i % 28),
+  content: "",
+  selectable: true,
+  type: "point",
+}));
+
 function App() {
   return (
     <div>
       <TimelinePicker
+        items={items10k}
         onSelect={(i) => console.log('Selected', i)}
         onDeselect={() => console.log('Deselected')}
       />

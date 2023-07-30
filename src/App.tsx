91c3a27e8fa12ea71d9039392cd7d4f6157d5a53
Elliot Sayes <elliot@sayes.cloud>
2023-07-30 21:54:31 +0800

    FIXED!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import TimelinePicker from './components/TimelinePicker'
+// import TimelineTest from './components/TimelineTest'
 
 function App() {
   return (
-    <>
-      <TimelinePicker />
-    </>
+    <div>
+      <TimelinePicker onSelect={(i) => console.log('Selected', i)} />
+    </div>
   )
 }
 
--- src/components/TimelinePicker.tsx
+++ src/components/TimelinePicker.tsx
@@ -1,75 +1,104 @@
-import { useEffect, useRef } from 'react';
-import type { TimelineItem, TimelineOptions } from 'vis-timeline/types';
-import Timeline from 'react-lms-vis-timeline';
+import { useEffect, useRef, useState } from 'react';
+import { Timeline, type TimelineItem, type TimelineOptions } from 'vis-timeline/esnext';
 import 'vis-timeline/styles/vis-timeline-graph2d.css';
 import './TimelinePicker.css'
-
 
 const items: TimelineItem[] = [{
   id: 0,
   start: new Date(2020, 0, 0),
-  content: 'Trajectory A',
+  content: '',
   selectable: true,
   type: 'point',
 }, {
   id: 1,
   start: new Date(2023, 0, 0),
-  content: 'Trajectory B',
+  content: '',
   selectable: true,
   type: 'point',
 }]
 
-const TimelinePicker = () => {
+const nowMs = Date.now()
+const firstInteraction = items[0].start! as Date
+// const lastInteraction = (items[items.length - 1].end ?? items[items.length - 1].start) as Date
+const duration = nowMs - firstInteraction.getTime()
+const clampedDuration = Math.max(
+  duration,
+  1000 * 60 * 60 * 24, // 1 day
+);
+const min = new Date(firstInteraction.getTime() - clampedDuration * 0.05);
+const max = new Date(nowMs + clampedDuration * 0.05);
+const options: TimelineOptions = {
+  min,
+  max,
+  width: '800px',
+  height: '150px',
+  showMajorLabels: true,
+  showCurrentTime: true,
+  zoomMin: 10000000,
+  format: {
+    minorLabels: {
+      minute: 'h:mma',
+      hour: 'ha'
+    }
+  },
+  onAdd: (item) => console.log(item),
+  zoomFriction: 5,
+}
 
-  const nowMs = Date.now()
-  const firstInteraction = items[0].start! as Date
-  // const lastInteraction = (items[items.length - 1].end ?? items[items.length - 1].start) as Date
-  const duration = nowMs - firstInteraction.getTime()
-  const clampedDuration = Math.max(
-    duration,
-    1000 * 60 * 60 * 24, // 1 day
-  );
-  const min = new Date(firstInteraction.getTime() - clampedDuration * 0.05);
-  const max = new Date(nowMs + clampedDuration * 0.05);
+interface Props {
+  onSelect?: (item?: number) => void,
+  onDeselect?: () => void,
+}
 
-  const options: TimelineOptions = {
-    min,
-    max,
-    width: '100%',
-    height: '150px',
-    showMajorLabels: true,
-    showCurrentTime: true,
-    zoomMin: 10000000,
-    format: {
-      minorLabels: {
-        minute: 'h:mma',
-        hour: 'ha'
-      }
-    },
-    onAdd: (item) => console.log(item),
-    zoomFriction: 5,
-  }
+const TimelinePicker = (props: Props) => {
+  const { onSelect, onDeselect } = props;
 
-  const timelineRef = useRef<Timeline>(null)
+  const timelineDivRef = useRef<HTMLDivElement>(null);
+  const loadRef = useRef(false);
+  const setupRef = useRef(false);
+  const [_, setTimeline] = useState<Timeline | null>(null)
 
   useEffect(() => {
-    setTimeout(() => {
-      timelineRef.current?.timeline.zoomOut(1)
-      timelineRef.current?.timeline.setItems(items)
-      timelineRef.current?.timeline.redraw()  
-    }, 0);
-  }, [timelineRef])
+    if (loadRef.current || setupRef.current) {
+      console.log('returning')
+      return
+    }
+    loadRef.current = true;
+    console.log('setting up')
+
+    const timeline = new Timeline(timelineDivRef.current!, items, options)
+    type SelectProperties = {
+      items: number[];
+    }
+    timeline.on('select', (properties: SelectProperties) => {
+      if (properties.items.length === 0) {
+        onDeselect?.()
+      } else {
+        onSelect?.(properties.items[0])
+      }
+    })
+    timeline.fit()
+    setTimeline(timeline)
+    // timeline.zoomOut(1)
+    // timeline.redraw()
+    
+    console.log('setup complete')
+    setupRef.current = true;
+    loadRef.current = false;
+
+    return () => {
+      console.log('destroying')
+      timeline.off('select')
+      timeline.destroy()
+      setupRef.current = false
+    }
+  }, [])
 
   return (
     <div className="w-[800px] delay-75 animate-fadeIn relative">
       <div className='absolute z-50 left-0 top-0 bottom-0 w-[10%] bg-gradient-to-r from-gray-900/20 bg-blend-overlay pointer-events-none' />
       <div className='absolute z-50 right-0 top-0 bottom-0 w-[10%] bg-gradient-to-l from-gray-900/20 bg-blend-overlay pointer-events-none' />
-      <Timeline
-        ref={timelineRef}
-        initialItems={items}
-        options={options}
-        currentTime={nowMs}
-      />
+      <div ref={timelineDivRef} />
     </div>
   )
 }

2aa2189bc4a8f13d75ba5aa8f623ad1d5c0e8eaf
Elliot Sayes <elliot@sayes.cloud>
2023-07-31 05:09:53 +0800

    Faster fade-in

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,11 @@ export default {
   theme: {
     extend: {
       animation: {
-        "fade-in": "fade-in 2s ease-in-out",
+        "fade-in": "fade-in 1s ease-in-out",
       },
       keyframes: {
         "fade-in": {
           "0%": { opacity: 0 },
-          "25%": { opacity: 0 },
           "100%": { opacity: 1 },
         },
       },

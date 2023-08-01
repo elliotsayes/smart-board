795c1a4cc658e0d4accb99d6ae333e16b28c6408
Elliot Sayes <elliot@sayes.cloud>
2023-08-01 13:28:15 +0800

    Create Dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react"
+import { ContractDataFull } from "../types"
+import { useMachine } from "@xstate/react"
+import { dashboardMachine } from "../machines/dashboard"
+import TimelinePicker from "./TimelinePicker"
+
+interface Props {
+  contractData: Partial<ContractDataFull>
+}
+
+const Dashboard = ({ contractData }: Props) => {
+  const [current, send] = useMachine(() => dashboardMachine());
+
+  useEffect(() => {
+    send({ type: 'Update Contract Data', data: { contractData }})
+  }, [send, contractData])
+
+  return (
+    <>
+      <div>
+        {JSON.stringify(current.value)}
+      </div>
+      <div>
+        {
+          contractData.interactionHistory && (
+            <TimelinePicker
+              items={
+                contractData.interactionHistory.map((interaction) => ({
+                  id: interaction.id,
+                  content: '',
+                  start: interaction.block.timestamp,
+                }))
+              }
+            />
+          )
+        }
+      </div>
+    </>
+  )
+}
+
+export default Dashboard
---
+++ src/machines/dashboard.ts
@@ -0,0 +1,58 @@
+import { createMachine } from "xstate";
+import { ContractDataFull } from "../types";
+
+type Context = {
+  contractData: Partial<ContractDataFull>;
+  viewportTab: number;
+  selectedInteractionIndex?: number;
+};
+
+type Event = {
+  type: "Update Contract Data";
+  data: {
+    contractData: Partial<ContractDataFull>;
+  };
+};
+
+export const dashboardMachine = () =>
+  createMachine({
+    /** @xstate-layout N4IgpgJg5mDOIC5QQIawBYCMD2KBOEAdAJIB2AlgC7koA2AxANoAMAuoqAA7axXnakOIAB6IALAE5CANgCMAdgAcYgKwqxs2dJXyJAGhABPRACZmKwrOYBmJc0WKV1rRIC+rg6gw58RACJoWLgEhAGUKCQQtGD0AKqcqJRgAAQAwgKUeCgAxpTJYSgs7Egg3LzUAkKiCCaK0oTWYnVqttJ1NgbGCCqyioS1SkoS0syy1g7uHiCk2BBwQl5BvkJlfJUl1QC00lJNmmYm8irSSp2Ix4Q9YtYmYswaEvYmsu6egT4hZHx0KzxrghtxIozghZGIxK8QIsPv53sEIL9yvwAaAts9CHtZAcjiddBIVCDatZCBIJFj5DdHMwTpDofDQnDfKFyLBOLQUF0uH8KiiRKZ6j0HLp5LcbmZzCDbMwMYp7PJmCYrnVbrTGSEAt56QVEf8qogFPVFCYdmIKSpjfiCUZEFKZXKFUrpCZVZqmRqluqUOFItEdTy9TVboQjSazRa1CDVPUJPKxCYbo0mk1Jq4gA */
+    id: "dashboard",
+    tsTypes: {} as import("./dashboard.typegen").Typegen0,
+    schema: {
+      context: {} as Context,
+      events: {} as Event,
+    },
+    context: {
+      viewportTab: 0,
+      contractData: {},
+    },
+    initial: "Initial",
+    states: {
+      Initial: {
+        always: "Dashboard",
+      },
+
+      Dashboard: {
+        states: {
+          Display: {},
+          Data: {
+            states: {
+              Idle: {
+                on: {
+                  "Update Contract Data": {
+                    target: "Idle",
+                    internal: true,
+                  },
+                },
+              },
+            },
+
+            initial: "Idle",
+          },
+        },
+
+        type: "parallel",
+      },
+    },
+  });

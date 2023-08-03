328fad2896625ce2ea00e1fe9101a3e2768ddbce
Elliot Sayes <elliot@sayes.cloud>
2023-08-03 14:32:34 +0800

    Fix style

--- src/components/DashboardBox.tsx
+++ src/components/DashboardBox.tsx
@@ -8,7 +8,7 @@ const DashboardComponent = (props: Props) => {
   const { loading, padding, children } = props;
   return (
     <div className="h-full w-full rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
-      <div className={`flex h-full w-full items-center justify-center bg-gray-800 rounded-md back overflow-scroll ${padding !== false && "p-4"}`}>
+      <div className={`flex h-full w-full items-center justify-center bg-gray-800 rounded-md back overflow-clip ${padding !== false && "p-4"}`}>
         {
           loading ? (
             <p>Loading...</p>

ca3e96d98ddc56d9e6a136249a8b3dbeb807c402
Elliot Sayes <elliot@sayes.cloud>
2023-08-03 16:41:07 +0800

    Same colours

--- src/components/DashboardBox.tsx
+++ src/components/DashboardBox.tsx
@@ -7,7 +7,7 @@ interface Props {
 const DashboardComponent = (props: Props) => {
   const { loading, padding, children } = props;
   return (
-    <div className="h-full w-full rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+    <div className="h-full w-full rounded-lg bg-gradient-to-r from-[#D56DFB] to-[#0085FF] p-1">
       <div className={`flex h-full w-full items-center justify-center bg-gray-800 rounded-md back overflow-clip ${padding !== false && "p-4"}`}>
         {
           loading ? (

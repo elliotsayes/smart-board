7f8fd072ad9f9b8a5d271e48823b895cc02e150e
Elliot Sayes <elliot@sayes.cloud>
2023-08-03 11:03:52 +0800

    Dashboard box

---
+++ src/components/DashboardBox.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import DashboardBox from "./DashboardBox";
+import MouseTester from "./MouseTester";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "App/DashboardBox",
+  component: DashboardBox,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    //
+  },
+} satisfies Meta<typeof DashboardBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    loading: false,
+    padding: "default",
+    children: <MouseTester />,
+  },
+};
---
+++ src/components/DashboardBox.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  loading: boolean;
+  padding?: "default" | "none";
+  children?: React.ReactNode;
+}
+
+const DashboardComponent = (props: Props) => {
+  const { loading, padding, children } = props;
+  return (
+    <div className="h-full w-full rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+      <div className={`flex h-full w-full items-center justify-center bg-gray-800 rounded-md back ${padding != "none" && "p-4"}`}>
+        {
+          loading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              {children}
+            </>
+          )
+        }
+      </div>
+    </div>
+  );
+};
+
+export default DashboardComponent;

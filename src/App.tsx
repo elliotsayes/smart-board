92e082b4ee7565063145258597df928472c044d4
Elliot Sayes <elliot@sayes.cloud>
2023-08-04 11:38:02 +0800

    busy

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,13 @@ import ContractManager from './components/ContractManager';
 // import MouseTester from './components/MouseTester';
 
 const quiet = 'aeQgDoPgdixT7tjNXC9X4x6NRjGjAmsvUndl3_EHlto'
-// const busy = '-8A6RexFkpfWwuyVO98wzSFZh0d6VJuI-buTJvlwOJQ'
+const busy = '-8A6RexFkpfWwuyVO98wzSFZh0d6VJuI-buTJvlwOJQ'
+const contractId = import.meta.env.DEV ? quiet : busy;
 
 function App() {
   return (
     <div>
-      <ContractManager initialContractId={quiet} />
+      <ContractManager initialContractId={contractId} />
     </div>
   )
 }

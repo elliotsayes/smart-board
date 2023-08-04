6880540954c3213d4069dc1b04cdd21d0bd33768
Elliot Sayes <elliot@sayes.cloud>
2023-08-04 09:42:07 +0800

    Fix title

--- src/components/HashView.stories.ts
+++ src/components/HashView.stories.ts
@@ -4,7 +4,7 @@ import HashView from "./HashView";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: "App/HashText",
+  title: "App/HashView",
   component: HashView,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout

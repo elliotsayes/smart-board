b2f548a575963a15e3c798e808d9acb1caacc108
Elliot Sayes <elliot@sayes.cloud>
2023-08-05 20:56:32 +0800

    Rounding

--- src/components/DashboardBox.tsx
+++ src/components/DashboardBox.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 const DashboardComponent = ({children, loading = false, padding = true }: Props) => {
   return (
-    <div className="h-full w-full rounded-lg bg-gradient-to-r from-[#D56DFB] to-[#0085FF] p-1">
-      <div className={`flex h-full w-full items-center justify-center bg-gray-800 rounded-md back overflow-clip ${padding && "p-4"}`}>
+    <div className="h-full w-full rounded-b-xl rounded-tr-xl bg-gradient-to-r from-[#D56DFB] to-[#0085FF] p-1">
+      <div className={`flex h-full w-full items-center justify-center bg-gray-800 rounded-b-lg rounded-tr-lg back overflow-clip ${padding && "p-4"}`}>
         {
           loading ? (
             <p>Loading...</p>

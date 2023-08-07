a1b035e3f9d4f101ab12cbd8293a8055cfd51bb4
Elliot Sayes <elliot@sayes.cloud>
2023-08-07 10:52:30 +0800

    Tweak width

--- src/components/InteractionListView.tsx
+++ src/components/InteractionListView.tsx
@@ -92,8 +92,8 @@ const InteractionListView = ({items, /*selectedInteractionIndex,*/ onSelect}: Pr
   return (
     <div className="p-2">
       <div className="h-2" />
-      <div ref={tableContainerRef} className="h-[500px] max-w-lg overflow-auto">
-        <table className=" border-collapse table-fixed w-[100%]">
+      <div ref={tableContainerRef} className="h-[500px] overflow-auto">
+        <table className="border-collapse table-fixed w-[100%]">
           <thead className="sticky top-0 m-0">
             {table.getHeaderGroups().map(headerGroup => (
               <tr key={headerGroup.id}>
